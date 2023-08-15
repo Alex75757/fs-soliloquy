1fbdd939759195f0e9f23bd0f060d4b8b7be5414
Alex Kovalenko <arkov@mail.ru>
2023-08-15 11:42:07 +0300

    server to 3001

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ dotenv.config();
 
 import app from './app.js';
 
-const port = process.env.SERVER_PORT || 3000;
+const port = process.env.SERVER_PORT || 3002;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}.`);
